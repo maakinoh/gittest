8b400fa18cd56541df58971a561d8d86cee36690
Hannes Kuss <hkuss@uni-bremen.de>
2024-02-01 14:20:40 +0100

    First test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LibGit2Sharp;
+
+Console.WriteLine("Hello, World!");
+
+var repo = new Repository("./.git");
+
+
+foreach (var commit in repo.Commits)
+{
+    var parents = commit.Parents;
+    foreach (var parent in parents)
+    {
+        var diff = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
+        foreach (var ad in diff.Added)
+        {
+            Console.WriteLine(ad.Path);
+        }
+    }
+}
