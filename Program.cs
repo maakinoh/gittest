3e78a4c0a541b4b39658b548806bf8b1368588e5
Hannes Kuss <hkuss@uni-bremen.de>
2024-02-01 14:37:22 +0100

    nochwas

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using LibGit2Sharp;
 
 Console.WriteLine("Hello, World!");
 
-var repo = new Repository("./.git");
+var repo = new Repository("/Users/hanneskuss/Dev/gittest/");
+
+Dictionary<String, Dictionary<String,int>> fileHist = new Dictionary<string, Dictionary<String,int>>();
 
 foreach (var commit in repo.Commits)
 {
@@ -13,6 +15,26 @@ foreach (var commit in repo.Commits)
         var diff = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
         foreach (var ad in diff.Added)
         {
+            Console.WriteLine(ad.Path);
+        }
+        foreach (var ad in diff.Modified)
+        {
+            if (fileHist.ContainsKey(ad.Path))
+            {
+                if (fileHist[ad.Path].ContainsKey(commit.Author.Email))
+                {
+                    fileHist[ad.Path][commit.Author.Email] += 1;
+                }
+                else
+                {
+                    fileHist[ad.Path][commit.Author.Email] = 1;
+                }
+            }
+            else
+            {
+                fileHist[ad.Path] = new Dictionary<string, int>();
+                fileHist[ad.Path][commit.Author.Email] = 1;
+            }
             Console.WriteLine(ad.Path);
         }
     }
